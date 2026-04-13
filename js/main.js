7fe3488bdd3f5fbdc07601b4e9680a565993efce
dat7371 <thanhdat110218@gmail.com>
2026-04-13 11:35:23 +0700

    Add: App pages(app-vrtech), product components, product page CSS, update header menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("componentsLoaded", () => {
   console.log("VRTECH landing loaded");
 });
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".site-header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
